1e030f52913b16a1d7a4014a9f34dc9bc8248001
DanikZab <152377873+DanikZab@users.noreply.github.com>
2024-01-22 16:57:30 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,29 @@
+const canvasPlot= document.getElementById('1')
+const ctx = 1.getContext('2d')
+ctx.fillRect(0,0,100,100);
+ctx.fillStyle='#0000ff';
+ctx.fillRect(100,100,200,200);
+
+ctx.moveTo(300,300);
+ctx.lineTo(400,400);
+ctx.stroke();
+
+ctx.strokeStyle='#ff0000'
+ctx.moveTo(320,300);
+ctx.lineTo(420,400);
+ctx.stroke();
+
+ctx.moveTo(300,300);
+ctx.lineTo(400,400);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle='#ff0000'
+ctx.moveTo(320,300);
+ctx.lineTo(420,400);
+ctx.stroke();
+
+ctx.fillText('текст',300,400);
+ctx.fillRect(300,400,2,2);
+ctx.font='30px Arial';
+ctx.textAlign='left';
+ctx.textBaseLine='top';
